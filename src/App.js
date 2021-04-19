2ae062b74ce2bd92b340c678479399d84b12c909
yuchianl <yuchianl007@gmail.com>
2021-04-19 14:28:04 +0800

    Create basic template

--- src/App.js
+++ src/App.js
@@ -2,21 +2,16 @@
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="grid-container">
+      <header>
+        <a href="/">React Shopping Cart</a>
       </header>
+      <main>
+        Product List
+      </main>
+      <footer>
+        All right is reserved.
+      </footer>
     </div>
   );
 }
